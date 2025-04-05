8e4e9f0087fe0748c24b548a5f2332edae063542
bnjnrthrp <benji.northrop@gmail.com>
2025-04-05 08:18:16 -0700

    Updated orbit camera so WASD orbits the object, arrow keys pan the center point, and QE will zoom in/out

--- src/scene.py
+++ src/scene.py
@@ -5,7 +5,7 @@ from pyrr import Matrix44, Vector3  # For matrix math
 from moderngl_window import WindowConfig, geometry
 from pathlib import Path
 from math import radians, cos, sin # For orbit camera
-
+from orbit_camera import OrbitCamera
 
 # pip install moderngl moderngl_window pywavefront
 
@@ -62,89 +62,16 @@ class Scene(WindowConfig):
         """,
         )
 
-        # vertices = np.array([
-		# # x, y, z, u, v
-
-		# # front face
-		# -1.0,  1.0,  1.0, 0.0, 1.0,
-		#  1.0, -1.0,  1.0, 1.0, 0.0,
-		#  1.0,  1.0,  1.0, 1.0, 1.0,
-		# -1.0,  1.0,  1.0, 0.0, 1.0,
-		# -1.0, -1.0,  1.0, 0.0, 0.0,
-		#  1.0, -1.0,  1.0, 1.0, 0.0,
-
-		# # back face
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-		#  1.0,  1.0, -1.0, 1.0, 1.0,
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		# -1.0, -1.0, -1.0, 0.0, 0.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-
-		# # left face
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		# -1.0, -1.0,  1.0, 1.0, 0.0,
-		# -1.0,  1.0,  1.0, 1.0, 1.0,
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		# -1.0, -1.0, -1.0, 0.0, 0.0,
-		# -1.0, -1.0,  1.0, 1.0, 0.0,
-
-		# # right face
-		#  1.0,  1.0,  1.0, 0.0, 1.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-		#  1.0,  1.0, -1.0, 1.0, 1.0,
-		#  1.0,  1.0,  1.0, 0.0, 1.0,
-		#  1.0, -1.0,  1.0, 0.0, 0.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-
-		#  # top face
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		#  1.0,  1.0,  1.0, 1.0, 0.0,
-		#  1.0,  1.0, -1.0, 1.0, 1.0,
-		# -1.0,  1.0, -1.0, 0.0, 1.0,
-		# -1.0,  1.0,  1.0, 0.0, 0.0,
-		#  1.0,  1.0,  1.0, 1.0, 0.0,
-
-		#  # bottom face
-		# -1.0, -1.0,  1.0, 0.0, 1.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-		#  1.0, -1.0,  1.0, 1.0, 1.0,
-		# -1.0, -1.0,  1.0, 0.0, 1.0,
-		# -1.0, -1.0, -1.0, 0.0, 0.0,
-		#  1.0, -1.0, -1.0, 1.0, 0.0,
-	
-        # ], dtype='f4')
-
         self.mesh = geometry.cube(attr_names=geometry.AttributeNames('in_position', texcoord_0='in_texcoord'))
-        # self.vbo = self.ctx.buffer(vertices.tobytes())
-        # self.vao = self.ctx.vertex_array(self.prog, [(self.vbo, '3f 2f', 'in_position', 'in_texcoord')])
         self.cube_pos = Matrix44.from_translation([0, 0, 0])
         self.start_time = 0.0
         
         # Setup orbit camera params
-        self.orbit_center = Vector3([0.0, 0.0, 0.0])
-        self.orbit_radius = 5.0
-        self.orbit_yaw = -90.0
-        self.orbit_pitch = 0.0
-        self.cam_up = Vector3([0., 1., 0.])
-
-        # Mouse settings
-
+        self.cam = OrbitCamera()
         self.cam_speed = 2.5 # Camera speed when moving
 
         # OpenCV webcam
         self.cap = cv2.VideoCapture(0)
-
-    def update_orbit_cam(self):
-        yaw_rad = radians(self.orbit_yaw)
-        pitch_rad = radians(self.orbit_pitch)
-
-        x = self.orbit_radius * cos(pitch_rad) * sin(yaw_rad)
-        y = self.orbit_radius * sin(pitch_rad)
-        z = self.orbit_radius * cos(pitch_rad) * cos(yaw_rad)
-
-        self.cam_pos = self.orbit_center + Vector3([x, y, z])
-        self.cam_tgt = self.orbit_center
 
     def on_render(self, time, frame_time):
         # Read a frame from webcam
@@ -160,22 +87,12 @@ class Scene(WindowConfig):
                 self.wnd.close()
         
         self.handle_movement(frame_time)
-        self.update_orbit_cam()
 
         self.ctx.clear(0.1, 0.1, 0.1)
         self.ctx.enable(self.ctx.DEPTH_TEST)
         
-        angle = time * 50
-        rotation = Matrix44.from_y_rotation(np.radians(angle))
-        model = self.cube_pos * rotation
-
-
-        view = Matrix44.look_at(
-            eye=self.cam_pos,
-            target=self.cam_tgt,
-            up=self.cam_up
-        )
-
+        model = self.cube_pos
+        view = self.cam.get_view_matrix()
         proj = Matrix44.perspective_projection(
             fovy=45.0,
             aspect=self.wnd.aspect_ratio,
@@ -198,43 +115,33 @@ class Scene(WindowConfig):
         keys = self.wnd.keys
         vel = self.cam_speed * dt
         rot_vel = 90.0 * dt
-        pan_vel = self.orbit_radius * 0.5 * dt
         zoom_vel = 2.0 * dt
 
-        # Orbit cam pan control
-        shift = self.wnd.is_key_pressed(keys.LEFT_SHIFT)
-        if shift:
-            yaw_rad = radians(self.orbit_yaw)
-            right = Vector3([cos(yaw_rad + np.pi / 2), 0, sin(yaw_rad + np.pi / 2)])
-            up = Vector3([0.0, 1.0, 0.0])
-
-            if self.wnd.is_key_pressed(keys.W):
-                self.orbit_center += up * pan_vel
-            if self.wnd.is_key_pressed(keys.S):
-                self.orbit_center -= up * pan_vel
-            if self.wnd.is_key_pressed(keys.A):
-                self.orbit_center -= right * pan_vel
-            if self.wnd.is_key_pressed(keys.D):
-                self.orbit_center += right * pan_vel
+        # Pan control        
+        if self.wnd.is_key_pressed(keys.UP):
+            self.cam.pan(0, 0, 1, vel)
+        if self.wnd.is_key_pressed(keys.DOWN):
+            self.cam.pan(0, 0, -1, vel)
+        if self.wnd.is_key_pressed(keys.LEFT):
+            self.cam.pan(-1, 0, 0, vel)
+        if self.wnd.is_key_pressed(keys.RIGHT):
+            self.cam.pan(1, 0, 0, vel)
 
         # Orbit cam rotation control
         if self.wnd.is_key_pressed(keys.W):
-            self.orbit_pitch += rot_vel
+            self.cam.rotate(0, -rot_vel)
         if self.wnd.is_key_pressed(keys.S):
-            self.orbit_pitch -= rot_vel
+            self.cam.rotate(0, rot_vel)
         if self.wnd.is_key_pressed(keys.A):
-            self.orbit_yaw -= rot_vel
+            self.cam.rotate(-rot_vel, 0)
         if self.wnd.is_key_pressed(keys.D):
-            self.orbit_yaw += rot_vel
-
-        # Clamp pitch to [-89, 89]
-        self.orbit_pitch = max(-89.0, min(89.0, self.orbit_pitch))
+            self.cam.rotate(rot_vel, 0)
 
         # Zoom Controls
         if self.wnd.is_key_pressed(keys.Q):
-            self.orbit_radius = max(1.0, self.orbit_radius - zoom_vel)
+            self.cam.zoom(-zoom_vel)
         if self.wnd.is_key_pressed(keys.E):
-            self.orbit_radius += zoom_vel
+            self.cam.zoom(zoom_vel)
 
     def destroy(self):
         # Clean up OpenCV when window closes
