5f8c420482eef827b4560eeed538bdce10b0aaf2
bnjnrthrp <benji.northrop@gmail.com>
2025-04-06 13:40:11 -0700

    Got the model manipulation functions working!!! The issue was with Python's default argument evaluator connecting scale and rotation to the same list

--- src/scene_object.py
+++ src/scene_object.py
@@ -3,12 +3,12 @@ from moderngl import Context, Program, Texture
 from math import radians
 
 class SceneObject:
-    def __init__(self, vao, texture: Texture, position=list([0, 0, 0]), scale=list([1, 1, 1]), rotation=list([0, 0, 0]), editable=False):
+    def __init__(self, vao, texture: Texture, position=None, scale=None, rotation=None, editable=False):
         self.vao = vao
         self.texture = texture
-        self.position = position
-        self.scale = scale
-        self.rotation = rotation
+        self.position = [0.0, 0.0, 0.0] if position is None else list(position)
+        self.scale = [1.0, 1.0, 1.0] if scale is None else list(scale)
+        self.rotation = [0.0, 0.0, 0.0] if rotation is None else list(rotation)
         self.editable = editable 
 
     def get_model_matrix(self):
@@ -18,6 +18,7 @@ class SceneObject:
         Rz = Matrix44.from_z_rotation(radians(self.rotation[2]))
         Sxyz = Matrix44.from_scale(self.scale)
 
+        # Matrix multiplication L <- R
         return Txyz @ Rz @ Ry @ Rx @ Sxyz
 
     def render(self, prog:Program, texture_unit=0, uv_scale=1.0):
